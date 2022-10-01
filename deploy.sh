244d00e27af681b07c66cb96bf686940b32f299b
Andy Fedchuk <andy@2solar.nl>
2022-10-01 12:33:31 +0300

    initial commit

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "Your bash version is $BASH_VERSION."
+
 # Verify AWS CLI Credentials are setup
 # http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
 if ! grep -q aws_access_key_id ~/.aws/config; then
@@ -13,8 +15,10 @@ fi
 
 read -r -p "Set deployment bucket name: " DEPLOYMENT_BUCKET
 read -r -p "Set AWS region: " REGION
+read -r -p "Set SES sender email: " SENDER
+read -r -p "Set SES email destination for notifications: " DESTINATION
 
-function createBucket(){
+function createBucket() {
   BUCKET_EXISTS=$(aws s3api head-bucket --bucket $DEPLOYMENT_BUCKET 2>&1 || true)
   if [ -z "$BUCKET_EXISTS" ]; then
     echo "Bucket $DEPLOYMENT_BUCKET already exists"
@@ -23,9 +27,9 @@ function createBucket(){
   fi
 }
 
-function tag(){
-	aws s3api put-bucket-tagging --bucket $DEPLOYMENT_BUCKET --tagging \
-	'{
+function tag() {
+  aws s3api put-bucket-tagging --bucket $DEPLOYMENT_BUCKET --tagging \
+    '{
 		"TagSet": [
 			{
 				"Key": "Lambda deployment bucket",
@@ -34,6 +38,43 @@ function tag(){
 		]
 	}'
 }
+aws sts get-caller-identity | jq .Account
+function sns() {
+  export TOPIC="ses-business-support"
+  arn=$(aws sns create-topic --name $TOPIC --output text || error_exit "Failed to create an SNS Topic, Check Output")
+}
+
+function strip_quotes() {
+  while [[ $# -gt 0 ]]; do
+    local value=${!1}
+    local len=${#value}
+    [[ ${value:0:1} == \" && ${value:$len-1:1} == \" ]] && declare -g $1="${value:1:$len-2}"
+    shift
+  done
+}
+
+accountId=$(aws sts get-caller-identity | jq .Account)
+strip_quotes accountId
+function alarm() {
+  export DAILY_SES_QUOTA=$(aws ses get-send-quota | jq .Max24HourSend)
+  declare -A metrics=([WARNING]=0.2 [ALERT]=0.5)
+  echo $accountId
+  for metric in "${!metrics[@]}"; do
+    $(
+      aws cloudwatch put-metric-alarm \
+        --alarm-name "${metric}: Daily SES Complaint Count as ${metrics[$metric]}%" \
+        --alarm-description "Daily SES Complaint Count is currently at ${metrics[$metric]}%, review complaints logs." \
+        --namespace "AWS/SES" \
+        --metric-name "Complaint" \
+        --statistic "Sum" \
+        --alarm-actions "arn:aws:sns:$REGION:$accountId:ses-business-support" \
+        --evaluation-periods 1 \
+        --comparison-operator "GreaterThanOrEqualToThreshold" \
+        --period "86400" \
+        --threshold $(printf '(%s+1)/1\n' "$DAILY_SES_QUOTA / 100 * ${metrics[$metric]}" | bc)
+    )
+  done
+}
 
 echo "Setup serverless..."
 npm install
@@ -41,5 +82,12 @@ npm install -g serverless
 echo "Creating bucket, creating tags..."
 createBucket
 tag
+
+echo "Setting up SNS to receive notifications..."
+sns
+
+echo "Setting up notifications metrics..."
+alarm
+
 echo "Deploying service to AWS.."
-sls deploy --param="bucket=$DEPLOYMENT_BUCKET"
+sls deploy --param="bucket=$DEPLOYMENT_BUCKET" --param="sender=$SENDER" --param="destination=$DESTINATION"
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
 const ses = new AWS.SESV2({apiVersion: '2019-09-27'});
+const sesV1 = new AWS.SES({apiVersion: '2019-09-27'});
 
 /**
  * Get SES identities
@@ -27,6 +28,57 @@ const getIdentities = async () => {
 };
 
 /**
+ * The result is a list of data points, representing the last two weeks of sending activity.
+ * @returns {Promise<{rejects, complaints, bounces, deliveryAttempts}|{rejects: number, complaints: number, bounces: number, deliveryAttempts: number}>}
+ */
+const getSendStatistics = async () => {
+  const sendStatistics = await sesV1.getSendStatistics().promise();
+  const defaultRates = {bounces: 0, complaints: 0, rejects: 0, deliveryAttempts: 0};
+
+  const daily = {};
+  const weekly = Object.assign({}, defaultRates);
+  sendStatistics.SendDataPoints
+    .sort((p, n) => n.Timestamp - p.Timestamp)
+    .map((item) => {
+      const date = item.Timestamp.toLocaleDateString();
+      if (!daily[date]) daily[date] =  Object.assign({}, defaultRates);
+      daily[date] = {
+        bounces: daily[date].bounces +  item.Bounces,
+        complaints: daily[date].complaints + item.Complaints,
+        rejects: daily[date].rejects + item.Rejects,
+        deliveryAttempts:  daily[date].deliveryAttempts + item.DeliveryAttempts,
+      };
+
+      weekly.bounces += item.Bounces;
+      weekly.complaints += item.Complaints;
+      weekly.deliveryAttempts +=  item.DeliveryAttempts;
+    });
+
+  const formatted = Object.keys(daily).map( (key, value) => {
+    const rates = {};
+    Object.keys(daily[key])
+      .map(k => (Object.assign(rates, { [k] : `${((daily[key][k]/ daily[key].deliveryAttempts) * 100).toFixed(2)}%` })));
+
+    return {
+      date: key,
+      ...rates,
+      deliveryAttempts: daily[key].deliveryAttempts,
+    };
+  });
+
+  return {
+    daily: formatted,
+    weekly: {
+      bounces: ((weekly.bounces / weekly.deliveryAttempts) * 100).toFixed(2) + '%',
+      complaints: ((weekly.complaints / weekly.deliveryAttempts) * 100).toFixed(2) + '%',
+      rejects: ((weekly.rejects / weekly.deliveryAttempts) * 100).toFixed(2) + '%',
+      deliveryAttempts: weekly.deliveryAttempts
+    }
+  };
+
+};
+
+/**
  * Get account quotes
  */
 const getQuotes = async () => {
@@ -35,15 +87,20 @@ const getQuotes = async () => {
   try {
     const quotes = await ses.getAccount().promise();
     const {EnforcementStatus, ProductionAccessEnabled} = quotes;
-    const {Max24HourSend, SentLast24Hours} = quotes.SendQuota;
+    const {Max24HourSend, SentLast24Hours, MaxSendRate} = quotes.SendQuota;
+    const {daily, weekly} = await getSendStatistics();
+
     result = {
       status: `AWS SES is ${EnforcementStatus.toLowerCase()}`,
       productionAccess: `Production access is ${ProductionAccessEnabled ? 'enabled' : 'disabled'}`,
       max24HourSend: `You have ${Max24HourSend} emails limit for last 24 hours.`,
       sentLast24Hours: `You already send ${SentLast24Hours} emails for last 24 hours.`,
+      maxSendRate: `The maximum number of emails that you can send per second is ${MaxSendRate}.`,
+      sendDailyRates: daily,
+      sendWeeklyRates: weekly
     };
   } catch (e) {
-    console.log(`Could not fetch SES account quotes.`);
+    console.log(`Could not fetch SES account quotes. ${JSON.stringify(e.message)}`);
   }
 
   return result;
@@ -100,21 +157,49 @@ const deleteIdentity = async (identity) => {
   }
 };
 
+export const sendNotificationHandler = async(event) => {
+  console.log(JSON.stringify(event));
+
+  const data = JSON.parse(event.Records[0].Sns.Message);
+  await ses.sendEmail({
+    FromEmailAddress:  process.env.SENDER,
+    Destination: {
+      ToAddresses: [ process.env.DESTINATION ]
+    },
+    Content: {
+      Simple: {
+        Subject: {
+          Charset: 'UTF-8',
+          Data: data.AlarmName,
+        },
+        Body: {
+          Text: {
+            Data: data.AlarmDescription,
+            Charset: 'UTF-8',
+          },
+        }
+      }
+    }
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: null,
+  };
+};
+
 /**
  * Handler for viewing quotes information
- * @param event
- * @param context
- * @param callback
  */
-exports.handler = async (event, context, callback) => {
+exports.handler = async () => {
   const quotes = await getQuotes();
   let headers = {"Content-Type": "application/json"};
 
-  callback(null, {
+  return {
     statusCode: 200,
     body: JSON.stringify(quotes),
     headers
-  });
+  };
 };
 
 /**
@@ -141,10 +226,10 @@ exports.identityHandler = async (event, context, callback) => {
     callback('Method not allowed.', {});
   }
 
-  callback(null, {
+  return {
     ...result,
     headers: {
       "Content-Type": "application/json"
     }
-  });
+  }
 };
